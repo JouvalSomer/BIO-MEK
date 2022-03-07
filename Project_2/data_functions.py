b5866cc47db6c39e5a27b5e3dd1f3bddef59bb3b
Jacobhudt <jacob.hudtwalcker@gmail.com>
2022-03-07 18:14:04 +0100

    endringer

--- Project_2/data_functions.py
+++ Project_2/data_functions.py
@@ -12,28 +12,29 @@ import numpy as np
 
 def pressure(G,pa,pv):
     n = int(np.sqrt(np.size(G)))
-    A = np.zeros((n+1,n+1))
-    A[0,0] = 1
-    A[n, n] = 1
-    for i in range(1,n):
+    A = np.zeros((n,n))
+    
+    for i in range(1,n-1):
         for j in range(0,n):
             if G[i,j] != 0:
                 A[i,j] = -1/G[i,j]
-                A[i,i] = A[i,i] + 1/G[i,j]
+                A[i,i] += 1/G[i,j]
                 
-    b = np.zeros(n+1)
+    A[0,0] = 1
+    A[n-1, n-1] = 1
+    b = np.zeros(n)
     b[0] = pa
-    b[n] = pv
+    b[int(n/2):] = pv
     p = np.linalg.solve(A,b)
-    return p
+    return p,A,b
 
 
 
 def flow_rates(G,p,R):
     n = np.size(p)
-    Q = np.zeros(n+1)
-    for i in range(0,n):
-        for j in range(0,n):
+    Q = np.zeros(n)
+    for i in range(0,n+1):
+        for j in range(0,n+1):
             if G[i,j] != 0:
                 Q[i,j] = np.abs(p[i]- p[j])/(R[i,j]*G[i,j])       
     return Q
@@ -43,9 +44,9 @@ def flow_rates(G,p,R):
 
 def average_speeds(G,Q,r):
     n = np.size(G)
-    v = np.zeros(n+1)
-    for i in range(0,n):
-        for j in range(0,n):
+    v = np.zeros(n)
+    for i in range(0,n+1):
+        for j in range(0,n+1):
             if G[i,j] != 0:
                 v[i,j] = Q[i,j]/(np.pi*r[i,j]**2)
     return v
--- Project_2/test.py
+++ Project_2/test.py
@@ -20,7 +20,7 @@ r= 1
 radius = new_radius(r)
 
 
-length_int =  0.5*10**(-3)
+length_int =  0.3#0.5*10**(-3)
 
 G = np.zeros((n+1,n+1))
 
@@ -29,20 +29,21 @@ for i in range(2,n):
     for j in range(2,n):
         if j % 2 == 0:
             #if i % 2 == 0:
-                G[int(j/2),j] = 1
-                G[int(j/2),j+1] = 2
+                G[int(j/2),j] = 0.3
+                G[int(j/2),j+1] = 0.4
                 
         if j % 2 == 0:
             #if i % 2 == 0:
-                G[j,int(j/2)] = 1
-                G[j+1,int(j/2)] = 2
+                G[j,int(j/2)] = 0.3
+                G[j+1,int(j/2)] = 0.4
         
 G[0,0] = 0
 G[n,n] = 0
 G[0,1] = G[1,0] = length_int
 
 
-p = pressure(G,1,2)
+
+p,A,b = pressure(G,1,0.1)
 
 print(G)
 
